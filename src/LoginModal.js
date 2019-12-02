e72c32597baf76e941d41d31d4e8858c387338d9
e136063 <joseph.decker@wnco.com>
2019-12-02 10:20:37 -0600

    Joseph - fixed issue when pressing enter on the form

--- src/LoginModal.js
+++ src/LoginModal.js
@@ -25,17 +25,17 @@ const LoginModal = props => {
     if (loginResult.length > 0) {
       loginResult[0] === "auth/invalid-email"
         ? updateUsernameError({
-            error: true,
-            message:
-              "Something went wrong with your email. Check its formatting"
-          })
+          error: true,
+          message:
+            "Something went wrong with your email. Check its formatting"
+        })
         : null;
 
       loginResult[0] === "auth/wrong-password"
         ? updatePasswordError({
-            error: true,
-            message: "Incorrect password. Try again"
-          })
+          error: true,
+          message: "Incorrect password. Try again"
+        })
         : null;
 
       console.log(loginResult[0]);
@@ -43,43 +43,58 @@ const LoginModal = props => {
     }
   }, [loginResult]);
 
+  const formLoginSubmit = (e) => {
+    console.log(e);
+    e.preventDefault();
+
+    firebaseApp
+      .auth()
+      .signInWithEmailAndPassword(username, password)
+      .catch(function (error) {
+        // Handle Errors here.
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        updateLoginResult([errorCode, errorMessage]);
+      });
+  }
+
   return (
     <Modal
-      open={props.modalState}
-      onClose={() => props.updateModalState(false)}
+      open={ props.modalState }
+      onClose={ () => props.updateModalState(false) }
       size="large"
     >
       <Modal.Content>
         <div
-          css={css`
-            height: 25vh;
+          css={ css`
+            height: 33vh;
             display: flex-inline;
             text-align: center;
             align-items: center;
           `}
         >
           <Grid
-            columns={2}
+            columns={ 2 }
             relaxed="very"
             textAlign="center"
             verticalAlign="middle"
           >
             <Grid.Row>
               <Grid.Column
-                css={css`
+                css={ css`
                   height: 100%;
                 `}
               >
                 <Header
                   as="h2"
-                  css={css`
+                  css={ css`
                     display: flex;
                     justify-content: center;
                     height: 60px;
                   `}
                 >
                   <div
-                    css={css`
+                    css={ css`
                       color: #000;
                       margin-left: 5px;
                       display: flex;
@@ -90,19 +105,23 @@ const LoginModal = props => {
                     Log-in to your account
                   </div>
                 </Header>
-                <Form
+                <Form type="" onSubmit={ e => {
+                  console.warn("I'm getting submitted!")
+                  formLoginSubmit(e)
+                } }
                   size="large"
-                  css={css`
-                    background-color: pink;
-                  `}
                 >
                   <Form.Input
                     fluid
                     icon="user"
                     iconPosition="left"
                     placeholder="Username"
-                    value={username}
-                    onChange={e => updateUserName(e.target.value)}
+                    value={ username }
+                    type="text"
+                    onChange={ e => {
+                      updateUserName(e.target.value)
+                      updateUsernameError(false)
+                    } }
                     error={
                       usernameError.error
                         ? { content: usernameError.message, pointing: "above" }
@@ -115,8 +134,11 @@ const LoginModal = props => {
                     iconPosition="left"
                     placeholder="Password"
                     type="password"
-                    value={password}
-                    onChange={e => updatePassword(e.target.value)}
+                    value={ password }
+                    onChange={ e => {
+                      updatePassword(e.target.value)
+                      updatePasswordError(false)
+                    } }
                     error={
                       passwordError.error
                         ? { content: passwordError.message, pointing: "above" }
@@ -124,30 +146,16 @@ const LoginModal = props => {
                     }
                   />
                   <Button
-                    onClick={() => props.updateModalState(false)}
-                    size="large"
-                    color="red"
-                    inverted
-                  >
-                    {/* <Icon name="remove" />  */}
-                    Cancel
-                  </Button>
-                  <Button
                     color="green"
                     inverted
                     size="large"
-                    onClick={() => {
-                      console.log("Logging user in!");
-                      firebaseApp
-                        .auth()
-                        .signInWithEmailAndPassword(username, password)
-                        .catch(function(error) {
-                          // Handle Errors here.
-                          let errorCode = error.code;
-                          let errorMessage = error.message;
-                          updateLoginResult([errorCode, errorMessage]);
-                        });
-                    }}
+                    type="submit"
+                    name="submit"
+                  // onClick={ (e) => {
+                  //   console.log("Logging user in!");
+                  //   formLoginSubmit(e)
+
+                  // } }
                   >
                     Login
                   </Button>
@@ -158,20 +166,20 @@ const LoginModal = props => {
                 </Message>
               </Grid.Column>
               <Grid.Column
-                css={css`
+                css={ css`
                   height: 100%;
                 `}
               >
                 <Header
                   as="h2"
-                  css={css`
+                  css={ css`
                     display: flex;
                     justify-content: center;
                     height: 60px;
                   `}
                 >
                   <div
-                    css={css`
+                    css={ css`
                       color: #000;
                       margin-left: 5px;
                       display: flex;
@@ -184,13 +192,13 @@ const LoginModal = props => {
                 </Header>
                 <div>
                   <Grid>
-                    <Grid.Row columns={3}>
+                    <Grid.Row columns={ 3 }>
                       <Grid.Column centered="true" floated="right">
                         <Button
                           color="facebook"
                           icon="facebook"
                           content="facebook"
-                          onClick={() => facebookLogin()}
+                          onClick={ () => facebookLogin() }
                         />
                       </Grid.Column>
                       <Grid.Column centered="true" width="5">
@@ -198,7 +206,7 @@ const LoginModal = props => {
                           color="twitter"
                           icon="twitter"
                           content="twitter"
-                          onClick={() => twitterLogin()}
+                          onClick={ () => twitterLogin() }
                         />
                       </Grid.Column>
                       <Grid.Column centered="true" floated="left">
@@ -206,7 +214,7 @@ const LoginModal = props => {
                           color="google plus"
                           icon="google"
                           content="google"
-                          onClick={() => googleLogin()}
+                          onClick={ () => googleLogin() }
                         />
                       </Grid.Column>
                     </Grid.Row>
@@ -215,7 +223,7 @@ const LoginModal = props => {
               </Grid.Column>
               <Divider
                 vertical
-                css={css`
+                css={ css`
                   height: 100%;
                 `}
               >
