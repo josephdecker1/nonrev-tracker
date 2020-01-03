d0fba6188cb02323a7e457cfe6130153b92facda
Joseph Decker <josephdecker1@users.noreply.github.com>
2020-01-02 22:46:30 -0700

    Feature/better home page (#3)
    * Joseph - got workspace attached, now gonna push the changes for deployment
    
    * Joseph - updating the homepage

--- src/apps/Home.js
+++ src/apps/Home.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { css } from "@emotion/core";
+import { Container } from "semantic-ui-react";
+
+import Loader from "../components/loader";
+import { colors } from "../app_css";
 
 const Home = props => {
   return (
-    <>
-      <h1>hello! Home page</h1>
-      {/* {Object.entries(props.user).map(key => {
-        console.log(key);
-        return (
-          <div key={key}>
-            {key[0].toString()} :: {key[1] ? key[1].toString() : "--"}
-          </div>
-        );
-      })} */}
-    </>
+    <Suspense fallback={<Loader />}>
+      <div
+        css={css`
+          background-color: ${colors.green};
+          width: 100vw;
+          padding: 10px;
+        `}
+      >
+        <h1>hello! Home page</h1>
+      </div>
+    </Suspense>
   );
 };
 
