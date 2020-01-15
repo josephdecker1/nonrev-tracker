2b678876e56457829f1b48d4668db158ea44caac
e136063 <joseph.decker@wnco.com>
2020-01-15 15:53:10 -0600

    Joseph - removed unneccesary code from userdata page

--- src/apps/UserData.js
+++ src/apps/UserData.js
@@ -1,12 +1,8 @@
 import React from "react";
 import { uuid } from "uuidv4";
 import { css } from "@emotion/core";
-import { Button, Table, Container } from "semantic-ui-react";
-import { uploadButton, uploadFormInput, colors } from "../app_css";
-import { getDistance, convertDistance, getCenterOfBounds } from "geolib";
-import { setLatLgnAirportsForFlights } from '../utils/flight_data'
-
-import airport_data from "../../airports.json";
+import { Table, } from "semantic-ui-react";
+import { colors } from "../app_css";
 
 const UserData = props => {
   const { user, userRef } = props;
@@ -14,62 +10,16 @@ const UserData = props => {
   const [flight_Data, updateFlight_Data] = React.useState([]);
   const [flightData, updateFlightData] = React.useState([]);
   const [flights_uploaded, updateFlightsUploaded] = React.useState(false);
-  const [mapCenterBounds, updateMapCenterBounds] = React.useState({});
-  const [totalDistanceTravelled, updateTotalDistanceTravelled] = React.useState(
-    0
-  );
-  const [uniqueAirports, updateUniqueAirports] = React.useState([]);
-  const [zoom, updateZoom] = React.useState(4);
-  const [airports, updateAirports] = React.useState(airport_data);
 
   React.useEffect(() => {
     userRef.get().then(doc => {
       if (doc.exists) {
-        console.log("USERDATA => " + JSON.stringify(doc.data()));
         updateUserData(doc.data());
         updateFlight_Data(doc.data().flight_data);
       }
     });
   }, [user]);
 
-  const handleChange = e => {
-    console.log(e.target.files[0]);
-
-    let jsonFile = e.target.files[0];
-    let reader = new FileReader();
-    reader.readAsText(jsonFile);
-
-    if (jsonFile) {
-      reader.onloadend = e => {
-        let updatedData = JSON.parse(reader.result);
-
-        updatedData = setLatLgnAirportsForFlights(updatedData)
-
-        let flight_center_bound = getCenterOfBounds(updatedData.flights_latlng);
-        console.log(updatedData.uniqueAirports);
-
-        updateFlightsUploaded(true);
-        updateMapCenterBounds({
-          lat: flight_center_bound.latitude,
-          lng: flight_center_bound.longitude
-        });
-        updateFlightData(updatedData.updatedData);
-        updateTotalDistanceTravelled(updatedData.milesTravelled);
-        updateUniqueAirports(updatedData.uniqueAirports);
-        updateZoom(5);
-      };
-    }
-  };
-
-  const handleSubmit = event => {
-    event.preventDefault();
-
-    userRef.update({ flight_data: flightData });
-    userRef.update({ totalDistanceTravelled: totalDistanceTravelled });
-    userRef.update({ uniqueAirports: uniqueAirports });
-
-    console.log(flightData);
-  };
 
   const renderData = data => {
     return (
@@ -124,9 +74,8 @@ const UserData = props => {
 
   return (
     <>
-      <h1>Hello, { userData?.user?.split(" ")[0] }, Here's your flight data.</h1>
+      <h1>Hey, here's your flight data.</h1>
       { Object.entries(userData).map(key => {
-        console.log(key);
         if (key[0] == "flight_data") {
           return (
             <div key={ `--${uuid()}` }>
